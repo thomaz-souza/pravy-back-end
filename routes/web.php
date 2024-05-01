27dd2434d98fc397954c81113ec3ccdf7d88bdb9
Thomaz Souza <thomazfelipe35@hotmail.com>
2024-05-01 13:03:44 -0700

    Factories e remoções

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-
-// Route::get('/', [UserController::class, 'index'])->name('users.index');
-
-// // Grupo de rotas para 'users'
-// Route::group(['prefix' => 'users'], function () {
-
-//     Route::get('/create', [UserController::class, 'create'])->name('users.create');
-//     Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-//     Route::post('/', [UserController::class, 'store'])->name('users.store');
-//     Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
-// });
