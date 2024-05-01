72948d934d2cd4206dec372697e91354a8d5948f
Thomaz Souza <thomazfelipe35@hotmail.com>
2024-04-30 19:03:54 -0700

    Criação de Controllers e Rotas Base

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Grupo de rotas para 'users'
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::post('/', [UserController::class, 'store'])->name('users.store');
+    Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
 });
