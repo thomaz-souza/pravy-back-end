19c3738c5c730aad353e34c367e4d6f1f7b031ac
Thomaz Souza <thomazfelipe35@hotmail.com>
2024-05-01 13:33:11 -0700

    Purge

--- resources/views/auth/register.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-
-@section('title', 'Criar')
-
-@section('content')
-
-@endsection
--- routes/web.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
