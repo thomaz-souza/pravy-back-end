21b0c4eba35a21dff07eb25c7ae98441610c4921
Thomaz Souza <thomazfelipe35@hotmail.com>
2024-05-01 12:43:52 -0700

    Deleção

--- resources/views/users/users.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Página Inicial')
-
-@section('content')
-    <h2>This is the Home Page</h2>
-    <p>Welcome to our brand new Laravel application.</p>
-@endsection
