adaf3cbf909d1e96c90109525e78b5eef8b9d5ce
Thomaz Souza <thomazfelipe35@hotmail.com>
2024-04-30 21:36:27 -0700

    Configurações Fortify

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
     }
 }
--- config/app.php
+++ config/app.php
@@ -122,10 +122,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-
-    'providers' => [
-        App\Providers\FortifyServiceProvider::class,
-    ],
-
 ];
