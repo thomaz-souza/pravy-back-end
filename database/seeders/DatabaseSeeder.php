10917359f29155c8f2b7b87cf54aa4c72be1d298
Thomaz Souza <thomazfelipe35@hotmail.com>
2024-05-02 14:17:05 -0700

    Atualização de seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use App\Models\Address;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(5)->create();
+
+        User::all()->each(function ($user) {
+            Address::factory()->count(rand(1, 3))->create(['user_id' => $user->id]);
+        });
     }
 }
